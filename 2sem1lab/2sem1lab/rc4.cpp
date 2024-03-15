53acd5ca8ece84cee3e5b6919afc725d39df9176
belplaton <platonbeliakov@gmail.com>
2024-03-16 00:16:14 +0300

    feat: rc4 encoder key init
    Key-Scheduling Algorithm для инициализации ключа

--- 2sem1lab/2sem1lab/rc4.cpp
+++ 2sem1lab/2sem1lab/rc4.cpp
@@ -7,28 +7,48 @@ class Encoder
 
 private:
 
-	vector<unsigned char> key;
+	const int _stateSize = 256;
+
+	vector<unsigned char> _state;
 
 	void rc4(vector<unsigned char>& data)
 	{
 
 	}
 
+	vector<unsigned char> ksa(unsigned char const* keyBytes, size_t keySize)
+	{
+		vector<unsigned char> state(_stateSize);
+		for (int n = 0; n < _stateSize; n++)
+		{
+			state[n] = n;
+		}
+
+		int j = 0;
+		for (int i = 0; i < _stateSize; i++)
+		{
+			j = (j + state[i] + keyBytes[i % keySize]) % _stateSize;
+			swap(state[i], state[j]);
+		}
+
+		return state;
+	}
+
 public:
 
 	Encoder(unsigned char const * keyBytes, size_t keySize)
 	{
-		key.assign(keyBytes, keyBytes + keySize);
+		_state = ksa(keyBytes, keySize);
 	}
 
 	~Encoder()
 	{
-		key.clear();
+		_state.clear();
 	}
 
 	Encoder(const Encoder& other)
 	{
-		key = other.key;
+		_state = other._state;
 	}
 
 	void encode(const char* inputFilePath, const char* outputFilePath, bool encrypt)
@@ -63,6 +83,7 @@ public:
 
 	inline void set_key(unsigned char const* keyBytes, size_t keySize)
 	{
-		key.assign(keyBytes, keyBytes + keySize);
+		_key.assign(keyBytes, keyBytes + keySize);
+		_state = ksa(_key);
 	}
 };
