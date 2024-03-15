d2f404de355ecde51d3c26d4eddd0d104b3485c8
belplaton <platonbeliakov@gmail.com>
2024-03-16 00:32:41 +0300

    fix && feat: PSRA and rc4 alghoritms
    Pseudo-Random Generator Alghoritm
    
    RC4

--- 2sem1lab/2sem1lab/rc4.cpp
+++ 2sem1lab/2sem1lab/rc4.cpp
@@ -9,46 +9,70 @@ private:
 
 	const int _stateSize = 256;
 
-	vector<unsigned char> _state;
+	vector<unsigned char> _key;
 
 	void rc4(vector<unsigned char>& data)
 	{
+		int i = 0;
+		int j = 0;
 
+		vector<unsigned char> state = ksa(_key);
+
+		for (int n = 0; n < data.size(); n++)
+		{
+			data[n] ^= prga(&state, &i, &j);
+		}
 	}
 
-	vector<unsigned char> ksa(unsigned char const* keyBytes, size_t keySize)
+	// Key-Scheduling Algorithm
+	vector<unsigned char> ksa(vector<unsigned char> key)
 	{
 		vector<unsigned char> state(_stateSize);
+
 		for (int n = 0; n < _stateSize; n++)
 		{
 			state[n] = n;
 		}
 
 		int j = 0;
+
 		for (int i = 0; i < _stateSize; i++)
 		{
-			j = (j + state[i] + keyBytes[i % keySize]) % _stateSize;
+			j = (j + state[i] + key[i % key.size()]) % _stateSize;
 			swap(state[i], state[j]);
 		}
 
 		return state;
 	}
 
+	// Pseudo-Random Generator Alghoritm
+	unsigned char prga(vector<unsigned char> *state, int *i, int *j)
+	{
+		*i = (*i + 1) % _stateSize;
+		*j = (*j + (*state)[*i]) % _stateSize;
+
+		swap(state[*i], state[*j]);
+
+		int pseudoRandomIndex = (*state)[((*state)[*i] + (*state)[*j]) % _stateSize];
+
+		return pseudoRandomIndex;
+	}
+
 public:
 
 	Encoder(unsigned char const * keyBytes, size_t keySize)
 	{
-		_state = ksa(keyBytes, keySize);
+		_key.assign(keyBytes, keyBytes + keySize);
 	}
 
 	~Encoder()
 	{
-		_state.clear();
+		_key.clear();
 	}
 
 	Encoder(const Encoder& other)
 	{
-		_state = other._state;
+		_key = other._key;
 	}
 
 	void encode(const char* inputFilePath, const char* outputFilePath, bool encrypt)
@@ -84,6 +108,5 @@ public:
 	inline void set_key(unsigned char const* keyBytes, size_t keySize)
 	{
 		_key.assign(keyBytes, keyBytes + keySize);
-		_state = ksa(_key);
 	}
 };
