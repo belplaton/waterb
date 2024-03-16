7a81704c8f68734f6f2559065bcb4ec13cfa813e
belplaton <platonbeliakov@gmail.com>
2024-03-16 23:52:48 +0300

    feat: logic values array completed

---
+++ 2sem1lab/2sem1lab/logval.cpp
@@ -0,0 +1,103 @@
+#include "logval.h"
+
+class logical_values_array
+{
+
+private:
+
+	unsigned int _value;
+
+public:
+
+	logical_values_array(unsigned int value = 0)
+	{
+		_value = value;
+	}
+
+	inline unsigned int get_value() const
+	{
+		return _value;
+	}
+
+	// Инверсия
+	inline logical_values_array inversion() const
+	{
+		return logical_values_array(~_value);
+	}
+
+	// Конъюнкция
+	inline logical_values_array conjunction(const logical_values_array& other) const
+	{
+		return logical_values_array(_value & other.get_value());
+	}
+
+	// Дизъюнкция
+	inline logical_values_array disjunction(const logical_values_array& other) const
+	{
+		return logical_values_array(_value | other.get_value());
+	}
+
+	// Имплекация
+	inline logical_values_array implication(const logical_values_array& other) const
+	{
+		return logical_values_array(inversion().get_value() | other.get_value());
+	}
+
+	// Коимпликация
+	inline logical_values_array coimplication(const logical_values_array& other) const
+	{
+		return logical_values_array(inversion().get_value() & other.get_value());
+	}
+
+	// Сложение по модулю 2
+	inline logical_values_array modulo_sum(const logical_values_array& other) const
+	{
+		return logical_values_array(_value ^ other._value);
+	}
+
+	// Эквиалентность
+	inline logical_values_array equivalence(const logical_values_array& other) const
+	{
+		return modulo_sum(other).inversion();
+	}
+
+	// Стрелка Пирса
+	inline logical_values_array pierce_arrow(const logical_values_array& other) const
+	{
+		return disjunction(other).inversion();
+	}
+
+	// Штрих Шиффера
+	inline logical_values_array sheffer_stroke(const logical_values_array& other) const
+	{
+		return conjunction(other).inversion();
+	}
+
+	// Значение бита по позиции
+	inline bool get_bit(int posisiton) const
+	{
+		// Сдвигаем нужный бит в самое право и выделяем его через побитовое умножение на 1 бит
+		return (_value >> posisiton) & 1;
+	}
+
+	// Сравнить два числа на эквиалентность
+	inline static bool equals(const logical_values_array& a, const logical_values_array& b)
+	{
+		// 0u - без знака при инверсии. ~0u - все битовые знаки - единицы
+		return a.equivalence(b).get_value() == ~0u;
+	}
+
+	void to_binary_string(char* binary_string, int string_size) const
+	{
+		int mask = 1 << (sizeof(unsigned char) * 8 - 1);
+
+		for (int i = 0; i < string_size; i++)
+		{
+			binary_string[i] = (_value & mask) ? '1' : '0';
+			mask >>= 1;
+		}
+
+		binary_string[string_size] = '\0';
+	}
+
+};
--- 2sem1lab/2sem1lab/main.cpp
+++ 2sem1lab/2sem1lab/main.cpp
@@ -1,12 +1,15 @@
 #include "rc4.cpp"
+#include "logval.cpp"
 
 using namespace std;
 
 void rc4_check();
+void logval();
 
 int main()
 {
-    rc4_check();
+    // rc4_check();
+    logval();
 
     return 0;
 }
@@ -27,4 +30,33 @@ void rc4_check()
     cout << "File \"rc4_encrypted.txt\" decrypted to \"rc4_decrypted.txt\" successfully!" << endl;
 
     cout << "\n[-- First exercise]" << endl;
+}
+
+void logval()
+{
+    logical_values_array a(14);
+    logical_values_array b(28);
+
+    cout << "a = " << a.get_value() << ", b = " << b.get_value() << endl;
+    cout << "a AND b = " << a.conjunction(b).get_value() << endl;
+    cout << "a OR b = " << a.disjunction(b).get_value() << endl;
+    cout << "a IMPLIES b = " << a.implication(b).get_value() << endl;
+    cout << "a COIMPLIES b = " << a.coimplication(b).get_value() << endl;
+    cout << "a MODULO_SUM b = " << a.modulo_sum(b).get_value() << endl;
+    cout << "a EQUIVALENT b = " << a.equivalence(b).get_value() << endl;
+    cout << "a PIERCE_ARROW b = " << a.pierce_arrow(b).get_value() << endl;
+    cout << "a SHEFFER_STROKE b = " << a.sheffer_stroke(b).get_value() << endl;
+
+    cout << "a equals b: " << logical_values_array::equals(a, b) << endl;
+
+    cout << "Bit at position 2 in b: " << b.get_bit(2) << endl;
+
+    int string_length = sizeof(unsigned char) * 8;
+    char* binary_string = (char*)malloc(string_length + 1);
+
+    a.to_binary_string(binary_string, string_length);
+
+    cout << "Binary representation of a: " << binary_string << endl;
+
+    free(binary_string);
 }
