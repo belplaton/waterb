ba2d7286810086241e4a99989d5ab69306b1bdd9
belplaton <platonbeliakov@gmail.com>
2024-05-05 19:29:39 +0300

    fix

--- 2sem2lab/2sem2lab/binary_operators.hpp
+++ 2sem2lab/2sem2lab/binary_operators.hpp
@@ -67,6 +67,7 @@ big_int big_int::operator << (unsigned int shift) const
 		for (auto j = 0; j < big_int::uint_size; j++)
 		{
 			int bit = get_bit(_digits[i], big_int::uint_size - j - 1);
+			if (i == 0) bit &= ~sign_bit_mask;
 			empty += bit > 0 ? 0 : 1;
 			if (bit)
 			{
@@ -76,9 +77,6 @@ big_int big_int::operator << (unsigned int shift) const
 				finished = true;
 				break;
 			}
-
-			std::cout << "I: " << i << " J: " << j << std::endl;
-
 		}
 
 		if (finished) break;
@@ -88,26 +86,20 @@ big_int big_int::operator << (unsigned int shift) const
 	//std::cout << "A: " << _digits.size() << " B: " << size << std::endl;
 
 	auto result_digits = std::vector<unsigned int>(size);
-	for (auto i = 0; i < _digits.size(); i++)
+	for (auto k = 0; k < _digits.size() * big_int::uint_size - (empty + macro_diff * big_int::uint_size); k++)
 	{
-		for (auto j = 0; j < big_int::uint_size; j++)
-		{
-			auto i1 = i + ((j + shift) - ((j + shift) % big_int::uint_size)) / big_int::uint_size;
-			auto j1 = (j + shift) % big_int::uint_size;
+		auto i1 = (k - k % big_int::uint_size) / big_int::uint_size;
+		auto i2 = ((k + shift) - (k + shift) % big_int::uint_size) / big_int::uint_size;
 
+		auto j1 = k % big_int::uint_size;
+		auto j2 = (k + shift) % big_int::uint_size;
+		auto num = i1 < _digits.size() ? _digits[_digits.size() - i1 - 1] : 0;
+		auto bit = get_bit(num, j1);
+		result_digits[size - i2 - 1] |= (bit << j2);
 
-			auto num = _digits[i];
-			if (i == 0) num &= ~sign_bit_mask;
-
-			//std::cout << "K: " << k << " I: " << i << " J: " << j << " I1: " << i1 << " J1: " << j1 << " SIZE: " << _digits.size() << std::endl;
-
-			if (i1 <= i)
-			{
-				auto bit = get_bit(num, (big_int::uint_size - j1 - 1));
-				result_digits[i1] |= bit << (big_int::uint_size - j - 1);
-			}
-		}
+		std::cout << "BIT: " << bit << " K: " << k << " I1: " << i1 << " J1: " << j1 << " I2: " << i2 << " J2: " << j2 << " SIZE1: " << _digits.size() << " SIZE2: " << size << std::endl;
 	}
+
 
 	result_digits[0] |= big_int::sign_bit_mask * is_negate(*this);
 
